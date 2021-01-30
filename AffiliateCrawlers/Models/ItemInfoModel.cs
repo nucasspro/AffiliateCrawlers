f8b34d2d1e9e2b01434f447da7aad4ebe54bd1d9
nucass <hailongdang.hailong@gmail.com>
2021-01-31 00:52:58 +0700

    Add new model to store data

---
+++ AffiliateCrawlers/Models/ItemInfoModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace AffiliateCrawlers.Models
+{
+    public class ItemInfoModel
+    {
+        public string Title { get; set; }
+        public string OriginalPrice { get; set; }
+        public string SalePrice { get; set; }
+        public string Content { get; set; }
+        public List<string> ImageLinks { get; set; }
+        public string Url { get; set; }
+    }
+}
